d208633625f9238a4576093e47c7e8d735c26d7c
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2024-01-06 13:26:01 +0530

    update: ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,63 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation();
+
+  const currentpath = location.pathname;
+
+  console.log(currentpath);
+
   return (
-    <div>
-      <ul className="flex">
-        <li className="py-4 my-1 font-bold hover:-translate-y-3 duration-500">
-          <Link
-            className="p-4 font-bold hover:-translate-y-3 duration-500"
-            to="/"
-          >
-            v-share
-          </Link>
-        </li>
-        <li className="py-4 my-1">
-          <Link
-            className="bg-gray-300 p-4 mx-1 rounded-md hover:bg-black hover:text-white duration-500 "
-            to="/"
-          >
-            Home
-          </Link>
-        </li>
-        <li className="py-4 my-1 relative">
-          <Link
-            className="bg-gray-300 p-4 mx-1 rounded-md hover:bg-black hover:text-white duration-500 "
-            to="/liveroom"
-          >
-            Live Room
-          </Link>
-          <span className="absolute top-0 right-0 bg-yellow-300 rounded-lg px-1">
-            Beta
-          </span>
-        </li>
-        <li className="py-4 my-1 mx-1">
-          <Link
-            className="bg-gray-300 p-4 rounded-md hover:bg-black hover:text-white duration-500 "
-            to="about"
-          >
-            About
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="border-b-2">
+        <ul className="md:flex">
+          <li className="md:py-1 my-1 font-bold hover:-translate-y-3 duration-500">
+            <Link
+              className="p-4 font-bold hover:-translate-y-3 duration-500 block"
+              to="/"
+            >
+              v-share
+            </Link>
+          </li>
+          <li className="md:py-1 my-1">
+            <Link
+              className={`p-4 mx-1 rounded-md duration-500 block ${
+                location.pathname === "/"
+                  ? "bg-black text-white"
+                  : "hover:bg-black hover:text-white bg-gray-300"
+              } `}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="md:py-1 my-1 relative">
+            <Link
+              className={`p-4 mx-1 rounded-md duration-500 block ${
+                location.pathname === "/liveroom"
+                  ? "bg-black text-white"
+                  : "hover:bg-black hover:text-white bg-gray-300"
+              } `}
+              to="/liveroom"
+            >
+              Live Room
+            </Link>
+          </li>
+          <li className="md:py-1 my-1 mx-1">
+            <Link
+              className={`p-4 mx-1 rounded-md duration-500 block ${
+                location.pathname === "/about"
+                  ? "bg-black text-white"
+                  : "hover:bg-black hover:text-white bg-gray-300"
+              } `}
+              to="about"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
--- src/components/PublishedText.tsx
+++ src/components/PublishedText.tsx
@@ -1,11 +1,15 @@
 import { doc, getDoc } from "firebase/firestore";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { db } from "../firebase";
 import { useEffect, useState } from "react";
 
 function PublishedText() {
   const { id } = useParams();
-  const [text, setText] = useState("Loading...");
+  const [text, setText] = useState<string>("Loading...");
+  const [textCopied, setTextCopied] = useState<boolean>(false);
+  const [linkCopied, setLinkCopied] = useState<boolean>(false);
+
+  const location = useLocation();
 
   useEffect(() => {
     const loadSharedText = async () => {
@@ -23,10 +27,40 @@ function PublishedText() {
     loadSharedText();
   }, [id]);
 
+  const copyText = () => {
+    navigator.clipboard.writeText(text);
+    setTextCopied(true);
+  };
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(
+      "https://vishesh-pandey.github.io/v-share/#" + location.pathname
+    );
+    setLinkCopied(true);
+  };
+
   return (
-    <div className="p-5">
-      <pre>{text}</pre>
-    </div>
+    <>
+      <div className="w-11/12 m-auto bg-gray-200">
+        <div className="controls p-5">
+          <button
+            onClick={copyText}
+            className="bg-gray-300 p-3 rounded-md hover:bg-yellow-100"
+          >
+            {textCopied ? "Text Copied" : "Copy Text"}
+          </button>
+          <button
+            onClick={copyLink}
+            className="bg-gray-300 p-3 rounded-md mx-3 hover:bg-yellow-100"
+          >
+            {linkCopied ? "Link Copied" : "Copy Link"}
+          </button>
+        </div>
+        <pre className="w-full p-5 bg-yellow-200 overflow-auto h-auto whitespace-break-spaces">
+          {text}
+        </pre>
+      </div>
+    </>
   );
 }
 
